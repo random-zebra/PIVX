5fcc27aa7c3e7dec1b43f1fc7fc3823307c68612
random-zebra <random.zebra@protonmail.com>
2019-05-04 10:54:13 +0200

    [Tests] Double stake - Block C

--- test/functional/p2p_pos_doublestake-splitchain.py
+++ test/functional/p2p_pos_doublestake-splitchain.py
@@ -19,22 +19,22 @@ class PoSDoubleStakeSplitChain(PIVX_FakeStakeTest):
     def run_test(self):
         self.description = "Covers the scenario of two valid PoS blocks with same height and same coinstake input."
         self.init_test()
-        INITAL_MINED_BLOCKS = 100
+        INITAL_MINED_BLOCKS = 300
 
         # 1) Starting mining blocks
         self.log.info("Mining %d blocks.." % INITAL_MINED_BLOCKS)
         self.node.generate(INITAL_MINED_BLOCKS)
 
-        # 2) Collect the possible prevouts
+        # 2) Collect 3 prevouts
         self.log.info("Collecting all unspent coins which we generated from mining...")
-        staking_utxo_list = self.node.listunspent()
+        staking_utxo_list = self.node.listunspent()[:3]
 
         # 3) Create the two blocks
 
         # -- A) get latest  block number and hash
         block_count = self.node.getblockcount()
         pastBlockHash = self.node.getblockhash(block_count)
-        self.log.info("Creating the two chains from common block n. %d (%s)" % (block_count, pastBlockHash))
+        self.log.info("Block count: %d - Current best: %s" % (self.node.getblockcount(), self.node.getbestblockhash()))
 
         # -- B) collect prevouts used for staking
         stakingPrevOuts = self.get_prevouts(staking_utxo_list, block_count)
@@ -59,6 +59,8 @@ class PoSDoubleStakeSplitChain(PIVX_FakeStakeTest):
         try:
             var = self.node.submitblock(bytes_to_hex_str(block_A.serialize()))
             time.sleep(2)
+            bestHash = self.node.getbestblockhash()
+            self.log.info("Block count: %d - Current best: %s" % (self.node.getblockcount(), bestHash))
             if var is not None:
                 self.log.info("result: %s" % str(var))
         except Exception as e:
@@ -68,10 +70,34 @@ class PoSDoubleStakeSplitChain(PIVX_FakeStakeTest):
         try:
             var = self.node.submitblock(bytes_to_hex_str(block_B.serialize()))
             time.sleep(2)
+            bestHash = self.node.getbestblockhash()
+            self.log.info("Block count: %d - Current best: %s" % (self.node.getblockcount(), bestHash))
             if var is not None:
                 self.log.info("result: %s" % str(var))
         except Exception as e:
             self.log.info("Exception: %s" % str(e))
+
+        # 5) Recollect prevouts
+        self.log.info("Recollecting unspent utxos...")
+        staking_utxo_list = self.node.listunspent()
+
+        # 6) Append another block to the forked chain
+        block_count += 1
+        pastBlockHash = block_B.hash if bestHash == block_A.hash else block_A.hash
+        stakingPrevOuts = self.get_prevouts(staking_utxo_list, block_count)
+
+        block_C = self.create_spam_block(pastBlockHash, stakingPrevOuts, block_count + 1)
+        self.log.info("Hash of Block C: %s" % block_C.hash)
+        try:
+            var = self.node.submitblock(bytes_to_hex_str(block_C.serialize()))
+            time.sleep(2)
+            self.log.info("Current tip: %d [besthash: %s]" % (
+                self.node.getblockcount(), bestHash))
+            if var is not None:
+                self.log.info("result: %s" % str(var))
+        except Exception as e:
+            self.log.info("Exception: %s" % str(e))
+
 
 
 
