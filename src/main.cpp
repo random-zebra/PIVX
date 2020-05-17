bffe216b57abf636adce32330832bfb232d576ca
random-zebra <random.zebra@protonmail.com>
2020-05-17 16:28:32 +0200

    [Consensus] allow transaction version 2 relay policy.
    This commit introduces a way to gracefully bump the default
    transaction version in a two step process.
    
    backports bitcoin@12c89c918534f8e615e80381b692d89d6b09d174

--- src/main.cpp
+++ src/main.cpp
@@ -711,7 +711,7 @@ unsigned int LimitOrphanTxSize(unsigned int nMaxOrphans)
 bool IsStandardTx(const CTransaction& tx, std::string& reason)
 {
     AssertLockHeld(cs_main);
-    if (tx.nVersion > CTransaction::CURRENT_VERSION || tx.nVersion < 1) {
+    if (tx.nVersion > CTransaction::MAX_STANDARD_VERSION || tx.nVersion < 1) {
         reason = "version";
         return false;
     }
