tmagik/catoshi

net: Add oneshot functions to CConnman

src/net.h
@@ -87,7 +87,6 @@ unsigned int SendBufferSize();
 
 typedef int NodeId;
 
-void AddOneShot(const std::string& strDest);
 CNode* FindNode(const CNetAddr& ip);
 CNode* FindNode(const CSubNet& subNet);
 CNode* FindNode(const std::string& addrName);
@@ -137,6 +136,7 @@ public:
     void GetBanned(banmap_t &banmap);
     void SetBanned(const banmap_t &banmap);
 
+    void AddOneShot(const std::string& strDest);
 private:
     struct ListenSocket {
         SOCKET socket;
@@ -171,6 +171,8 @@ private:
     bool setBannedIsDirty;
     bool fAddressesInitialized;
     CAddrMan addrman;
+    std::deque<std::string> vOneShots;
+    CCriticalSection cs_vOneShots;
 };
 extern std::unique_ptr<CConnman> g_connman;
 void MapPort(bool fUseUPnP);

random-zebra/PIVX

src/net.h
@@ -80,8 +80,8 @@ static const ServiceFlags REQUIRED_SERVICES = NODE_NETWORK;
 unsigned int ReceiveFloodSize();
 unsigned int SendBufferSize();
 
-void AddOneShot(std::string strDest);
 bool RecvLine(SOCKET hSocket, std::string& strLine);
+
 CNode* FindNode(const CNetAddr& ip);
 CNode* FindNode(const CSubNet& subNet);
 CNode* FindNode(const std::string& addrName);
@@ -130,6 +130,7 @@ public:
     void GetBanned(banmap_t &banmap);
     void SetBanned(const banmap_t &banmap);
 
+    void AddOneShot(const std::string& strDest);
 private:
     struct ListenSocket {
         SOCKET socket;
@@ -164,6 +165,8 @@ private:
     bool setBannedIsDirty;
     bool fAddressesInitialized;
     CAddrMan addrman;
+    std::deque<std::string> vOneShots;
+    RecursiveMutex cs_vOneShots;
 };
 extern std::unique_ptr<CConnman> g_connman;
 void MapPort(bool fUseUPnP);