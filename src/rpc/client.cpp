ac957fa6c67da0b53fd50530452eafead25613fe
furszy <matiasfurszyfer@protonmail.com>
2020-10-14 01:32:20 -0300

    introduce "reload_conf" argument in startmasternode "alias" RPC command.

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -132,6 +132,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
         {"submitbudget", 7},
         // disabled until removal of the legacy 'masternode' command
         //{"startmasternode", 1},
+        {"startmasternode", 3},
         {"mnvoteraw", 1},
         {"mnvoteraw", 4},
         {"setstakesplitthreshold", 0},
