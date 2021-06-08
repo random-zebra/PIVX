72cc826a335de9838157e9711e4e4abe50d1acca
random-zebra <random.zebra@protonmail.com>
2021-06-08 22:33:43 +0200

    [Trivial] Prefix all bls/quorum threads with pivx
    Makes it easier to find them. Note: had to s/quorum/q/ to fit into 15
    characters.

--- src/bls/bls_worker.cpp
+++ src/bls/bls_worker.cpp
@@ -65,7 +65,7 @@ void CBLSWorker::Start()
     workerCount = std::max(std::min(1, workerCount), 4);
     workerPool.resize(workerCount);
 
-    RenameThreadPool(workerPool, "bls-worker");
+    RenameThreadPool(workerPool, "pivx-bls-worker");
 }
 
 void CBLSWorker::Stop()
